1db436fe28a0abbe8cf402b08f729ac005276772
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-06-04 14:10:56 +0300

    First commit for unversioned files

---
+++ src/main/java/dev/bozlak/bbd_backend_2/BbdBackend2Application.java
@@ -0,0 +1,13 @@
+package dev.bozlak.bbd_backend_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbdBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbdBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bozlak/bbd_backend_2/BbdBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bozlak.bbd_backend_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbdBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
